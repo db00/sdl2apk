93bdec536a2709af10c3efcb1bcb0009ccedb360
db0 <db0@qq.com>
2016-10-31 14:38:02 +0800

    16.10.31

--- git.sh
+++ git.sh
@@ -13,11 +13,13 @@ git push test/master
 git remote rm test
 
 ssh-keygen -t rsa -C "db0@qq.com"
+#cp ~/.ssh/id_rsa.pub to github(Settings/SSH Keys)
 ssh -T git@github.com
 
 
 git config --global push.default simple
 git config --global user.name "db0"
 git config --global user.email db0@qq.com
+git push --set-upstream origin master
 
 

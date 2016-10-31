dbb2cc500cbe65f14046ff93bad1e1aea1a6b4a4
db0 <db0@qq.com>
2016-10-31 15:20:57 +0800

    16.10.31

--- git.sh
+++ git.sh
@@ -2,19 +2,23 @@ git init
 git add .
 git commit 
 
-git remote add test pi@rpi:~/sdl2apk
-git push -u test master
-
 git remote -v
 
-git pull (=git fetch test && git merge test/master)
-git push test/master
-
-git remote rm test
 
 ssh-keygen -t rsa -C "db0@qq.com"
 #cp ~/.ssh/id_rsa.pub to github(Settings/SSH Keys)
 ssh -T git@github.com
+
+
+#push an existing repository
+git remote add huaien pi@rpi:~/sdl2apk
+git push -u huaien master
+git remote -v
+git pull huaien
+
+
+git remote rm huaien
+
 
 
 git config --global push.default simple
@@ -23,8 +27,3 @@ git config --global user.email db0@qq.com
 git push --set-upstream origin master
 
 
-#push an existing repository
-git remote add huaien pi@rpi:~/sdl2apk
-git push -u huaien master
-git remote -v
-git pull huaien

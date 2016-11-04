b2e0a448f04965c7c64641427266f4561f32c8d5
db0 <db0@qq.com>
2016-11-04 16:04:53 +0800

    16.11.04

--- jni/src/SDL_gles2funcs.h
+++ jni/src/SDL_gles2funcs.h
@@ -1,29 +1,24 @@
 /*
-Simple DirectMedia Layer
-gcc -g -Wall -I"../SDL2/include/" -I"include" sprite.c ease.c tween.c mystring.c array.c files.c matrix.c -L"." -lopengl32  -lmingw32 -lSDL2main -lSDL2 -Ddebug_sprite && a
-Copyright (C) 1997-2014 Sam Lantinga <slouken@libsdl.org>
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2016 Sam Lantinga <slouken@libsdl.org>
 
-This software is provided 'as-is', without any express or implied
-warranty.  In no event will the authors be held liable for any damages
-arising from the use of this software.
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
 
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
 
-1. The origin of this software must not be misrepresented; you must not
-claim that you wrote the original software. If you use this software
-in a product, an acknowledgment in the product documentation would be
-appreciated but is not required.
-2. Altered source versions must be plainly marked as such, and must not be
-misrepresented as being the original software.
-3. This notice may not be removed or altered from any source distribution.
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
 */
-SDL_PROC(GLenum, glGetError, (void))
-SDL_PROC(GLuint, glCreateProgram, (void))
-SDL_PROC(GLuint, glCreateShader, (GLenum))
-SDL_PROC(GLint, glGetUniformLocation, (GLuint, const char *))
-SDL_PROC(GLint, glGetAttribLocation, (GLuint, const char*))
+
 SDL_PROC(void, glActiveTexture, (GLenum))
 SDL_PROC(void, glAttachShader, (GLuint, GLuint))
 SDL_PROC(void, glBindAttribLocation, (GLuint, GLuint, const char *))
@@ -32,6 +27,8 @@ SDL_PROC(void, glBlendFuncSeparate, (GLenum, GLenum, GLenum, GLenum))
 SDL_PROC(void, glClear, (GLbitfield))
 SDL_PROC(void, glClearColor, (GLclampf, GLclampf, GLclampf, GLclampf))
 SDL_PROC(void, glCompileShader, (GLuint))
+SDL_PROC(GLuint, glCreateProgram, (void))
+SDL_PROC(GLuint, glCreateShader, (GLenum))
 SDL_PROC(void, glDeleteProgram, (GLuint))
 SDL_PROC(void, glDeleteShader, (GLuint))
 SDL_PROC(void, glDeleteTextures, (GLsizei, const GLuint *))
@@ -41,34 +38,42 @@ SDL_PROC(void, glDrawArrays, (GLenum, GLint, GLsizei))
 SDL_PROC(void, glEnable, (GLenum))
 SDL_PROC(void, glEnableVertexAttribArray, (GLuint))
 SDL_PROC(void, glFinish, (void))
-//#ifdef linux
 SDL_PROC(void, glGenFramebuffers, (GLsizei, GLuint *))
-SDL_PROC(void, glShaderBinary, (GLsizei, const GLuint *, GLenum, const void *, GLsizei))
-SDL_PROC(void, glBindFramebuffer, (GLenum, GLuint))
-SDL_PROC(void, glFramebufferTexture2D, (GLenum, GLenum, GLenum, GLuint, GLint))
-SDL_PROC(GLenum, glCheckFramebufferStatus, (GLenum))
-SDL_PROC(void, glDeleteFramebuffers, (GLsizei, const GLuint *))
-//#endif
 SDL_PROC(void, glGenTextures, (GLsizei, GLuint *))
 SDL_PROC(void, glGetBooleanv, (GLenum, GLboolean *))
 SDL_PROC(const GLubyte *, glGetString, (GLenum))
+SDL_PROC(GLenum, glGetError, (void))
 SDL_PROC(void, glGetIntegerv, (GLenum, GLint *))
 SDL_PROC(void, glGetProgramiv, (GLuint, GLenum, GLint *))
 SDL_PROC(void, glGetShaderInfoLog, (GLuint, GLsizei, GLsizei *, char *))
 SDL_PROC(void, glGetShaderiv, (GLuint, GLenum, GLint *))
+SDL_PROC(GLint, glGetUniformLocation, (GLuint, const char *))
 SDL_PROC(void, glLinkProgram, (GLuint))
 SDL_PROC(void, glPixelStorei, (GLenum, GLint))
 SDL_PROC(void, glReadPixels, (GLint, GLint, GLsizei, GLsizei, GLenum, GLenum, GLvoid*))
 SDL_PROC(void, glScissor, (GLint, GLint, GLsizei, GLsizei))
-SDL_PROC(void, glShaderSource, (GLuint, GLsizei, const char **, const GLint *))
+SDL_PROC(void, glShaderBinary, (GLsizei, const GLuint *, GLenum, const void *, GLsizei))
+SDL_PROC(void, glShaderSource, (GLuint, GLsizei, const GLchar* const*, const GLint *))
 SDL_PROC(void, glTexImage2D, (GLenum, GLint, GLint, GLsizei, GLsizei, GLint, GLenum, GLenum, const void *))
 SDL_PROC(void, glTexParameteri, (GLenum, GLenum, GLint))
 SDL_PROC(void, glTexSubImage2D, (GLenum, GLint, GLint, GLint, GLsizei, GLsizei, GLenum, GLenum, const GLvoid *))
 SDL_PROC(void, glUniform1i, (GLint, GLint))
 SDL_PROC(void, glUniform4f, (GLint, GLfloat, GLfloat, GLfloat, GLfloat))
+SDL_PROC(void, glUniformMatrix4fv, (GLint, GLsizei, GLboolean, const GLfloat *))
 SDL_PROC(void, glUseProgram, (GLuint))
 SDL_PROC(void, glVertexAttribPointer, (GLuint, GLint, GLenum, GLboolean, GLsizei, const void *))
 SDL_PROC(void, glViewport, (GLint, GLint, GLsizei, GLsizei))
+SDL_PROC(void, glBindFramebuffer, (GLenum, GLuint))
+SDL_PROC(void, glFramebufferTexture2D, (GLenum, GLenum, GLenum, GLuint, GLint))
+SDL_PROC(GLenum, glCheckFramebufferStatus, (GLenum))
+SDL_PROC(void, glDeleteFramebuffers, (GLsizei, const GLuint *))
+SDL_PROC(GLint, glGetAttribLocation, (GLuint, const GLchar *))
+SDL_PROC(void, glGetProgramInfoLog, (GLuint, GLsizei, GLsizei*, GLchar*))
+SDL_PROC(void, glGenBuffers, (GLsizei, GLuint *))
+SDL_PROC(void, glBindBuffer, (GLenum, GLuint))
+SDL_PROC(void, glBufferData, (GLenum, GLsizeiptr, const GLvoid *, GLenum))
+SDL_PROC(void, glBufferSubData, (GLenum, GLintptr, GLsizeiptr, const GLvoid *))
+
 /**
 db0@qq.com added at 15.06.29 11:55:39
 */
@@ -89,13 +94,10 @@ SDL_PROC(void, glReleaseShaderCompiler,(void))
 SDL_PROC(void, glRenderbufferStorage,(GLenum target,  GLenum internalformat,  GLsizei width,  GLsizei height))
 //#endif
 
-SDL_PROC(void, glBindBuffer,(GLenum target,  GLuint buffer))
 SDL_PROC(void, glBlendColor,(GLclampf red,  GLclampf green,  GLclampf blue,  GLclampf alpha))
 SDL_PROC(void, glBlendEquation,(GLenum mode))
 SDL_PROC(void, glBlendEquationSeparate,(GLenum modeRGB,  GLenum modeAlpha))
 SDL_PROC(void, glBlendFunc,(GLenum sfactor,  GLenum dfactor))
-SDL_PROC(void, glBufferData,(GLenum target,  GLsizeiptr size,  const GLvoid * data,  GLenum usage))
-SDL_PROC(void, glBufferSubData,(GLenum target,  GLintptr offset,  GLsizeiptr size,  const GLvoid * data))
 SDL_PROC(void, glClearStencil,(GLint s))
 SDL_PROC(void, glColorMask,(GLboolean red,  GLboolean green,  GLboolean blue,  GLboolean alpha))
 SDL_PROC(void, glCompressedTexImage2D,(GLenum target,  GLint level,  GLenum internalformat,  GLsizei width,  GLsizei height,  GLint border,  GLsizei imageSize,  const GLvoid * data))
@@ -110,7 +112,6 @@ SDL_PROC(void, glDetachShader,(GLuint program,  GLuint shader))
 SDL_PROC(void, glDrawElements,(GLenum mode,  GLsizei count,  GLenum type,  const GLvoid * indices))
 SDL_PROC(void, glFlush,( void))
 SDL_PROC(void, glFrontFace,(GLenum mode))
-SDL_PROC(void, glGenBuffers,(GLsizei n,  GLuint * buffers))
 SDL_PROC(void, glGetFloatv,(GLenum pname,  GLfloat * params))
 SDL_PROC(void, glGetActiveAttrib,(GLuint program,  GLuint index,  GLsizei bufSize,  GLsizei *length,  GLint *size,  GLenum *type,  char *name))
 SDL_PROC(void, glGetActiveUniform,(GLuint program,  GLuint index,  GLsizei bufSize,  GLsizei *length,  GLint *size,  GLenum *type,  char *name))
@@ -156,13 +157,11 @@ SDL_PROC(void, glUniform3iv,(GLint location,  GLsizei count,  const GLint *value
 SDL_PROC(void, glUniform4iv,(GLint location,  GLsizei count,  const GLint *value))
 SDL_PROC(void, glUniformMatrix2fv,(GLint location,  GLsizei count,  GLboolean transpose,  const GLfloat *value))
 SDL_PROC(void, glUniformMatrix3fv,(GLint location,  GLsizei count,  GLboolean transpose,  const GLfloat *value))
-SDL_PROC(void, glUniformMatrix4fv, (GLint, GLsizei, GLboolean, const GLfloat *))
 SDL_PROC(void, glValidateProgram,(GLuint program))
 SDL_PROC(void, glVertexAttrib1f,(GLuint index,  GLfloat v0))
 SDL_PROC(void, glVertexAttrib2f,(GLuint index,  GLfloat v0,  GLfloat v1))
 SDL_PROC(void, glVertexAttrib3f,(GLuint index,  GLfloat v0,  GLfloat v1,  GLfloat v2))
 SDL_PROC(void, glVertexAttrib4f,(GLuint index,  GLfloat v0,  GLfloat v1,  GLfloat v2,  GLfloat v3))
-SDL_PROC(void, glGetProgramInfoLog,(GLuint program,  GLsizei maxLength,  GLsizei *length,  char *infoLog)) 
 SDL_PROC(void, glVertexAttrib1fv,(GLuint index,  const GLfloat *v))
 SDL_PROC(void, glVertexAttrib2fv,(GLuint index,  const GLfloat *v))
 SDL_PROC(void, glVertexAttrib3fv,(GLuint index,  const GLfloat *v))
